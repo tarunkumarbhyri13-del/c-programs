878349c256b0f021c0e902e98be3714eccf26d21
Tarun-kumar <tarunkumarbhyri13@gmail.com>
2026-04-26 22:28:28 +0530

    Add files via upload

---
+++ symbols.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char ch;
+    printf("Enter the character \n");
+    scanf("%c",&ch);
+    int res=!((ch>='A')&&(ch<='Z'))&&!((ch>='a')&&(ch<'z'))&&!((ch>='0')&&(ch<='9'));
+    printf("%c is a symbol i.e is:%d",ch,res);
+    return 0;
+}
